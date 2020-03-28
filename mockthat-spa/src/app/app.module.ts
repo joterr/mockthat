35cfb0bc22c0470d933f3d1b7398c4bfeac45829
Jannik Rulitschka <jannik.rulitschka@mhp.de>
2020-03-28 11:49:55 +0100

    Add basic editor functionality

--- mockthat-spa/src/app/app.module.ts
+++ mockthat-spa/src/app/app.module.ts
@@ -1,21 +1,21 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { FormsModule } from '@angular/forms';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { HeaderNavComponent } from './shared/components/header-nav/header-nav.component';
 import { FooterComponent } from './shared/components/footer/footer.component';
-import { SelectToolComponent } from './shared/components/header-nav/select-tool/select-tool.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     HeaderNavComponent,
-    FooterComponent,
-    SelectToolComponent
+    FooterComponent
   ],
   imports: [
     BrowserModule,
+    FormsModule,
     AppRoutingModule
   ],
   providers: [],
--- mockthat-spa/src/app/views/json-validator/json-validator.component.ts
+++ mockthat-spa/src/app/views/json-validator/json-validator.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, ElementRef, ViewChild } from '@angular/core';
 
 @Component({
     selector: 'app-json-validator',
@@ -6,13 +6,45 @@ import { Component, OnInit } from '@angular/core';
     styleUrls: [ './json-validator.component.scss' ]
 })
 export class JsonValidatorComponent implements OnInit {
+    @ViewChild('rawJson') rawJson: ElementRef;
+    @ViewChild('formatted') formatted: ElementRef;
+
     isValid: boolean;
     hasResult: boolean;
+
+    showTypes: boolean;
 
     constructor() { }
 
     ngOnInit(): void {
         this.isValid = false;
+
+        window.setTimeout(() => {
+            this.rawJson.nativeElement.focus();
+        });
     }
 
+    rawJsonChanged(value: KeyboardEvent): void {
+        const text: string = this.rawJson.nativeElement.value;
+        this.hasResult = !!text;
+
+        if (/^[\],:{}\s]*$/.test(text.replace(/\\["\\\/bfnrtu]/g, '@').
+            replace(/"[^"\\\n\r]*"|true|false|null|-?\d+(?:\.\d*)?(?:[eE][+\-]?\d+)?/g, ']').
+            replace(/(?:^|:|,)(?:\s*\[)+/g, ''))) {
+            this.rawJson.nativeElement.innerHTML = JSON.stringify(text);
+            this.formatted.nativeElement.innerHTML = JSON.stringify(text);
+            this.isValid = true;
+        } else {
+            this.isValid = false;
+            this.formatted.nativeElement.innerHTML = null;
+        }
+    }
+
+    copyFormatted(): void {
+
+    }
+
+    toggleTypes(): void {
+        this.showTypes = !this.showTypes;
+    }
 }
