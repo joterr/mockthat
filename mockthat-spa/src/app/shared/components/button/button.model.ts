73163179f66059bd07a1fda732aba8eb241523f9
Jannik Rulitschka <jannik.rulitschka@mhp.de>
2020-04-16 14:02:11 +0200

    Add button component

---
+++ mockthat-spa/src/app/shared/components/button/button.model.ts
@@ -0,0 +1,9 @@
+export enum ButtonLevel {
+    FIRST = 'first',
+    SECOND = 'second'
+}
+
+export enum ButtonType {
+    TOGGLE_VISIBILITY = 'toggle-visibility',
+    MICRO_FEEDBACK = 'micro-feedback'
+}
